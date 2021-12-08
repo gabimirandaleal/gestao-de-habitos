3533920ab82d544e2bc28da518de38e4d0bfec05
Gabriela Leal <gabrielamiranda@alu.ufc.br>
2021-12-08 20:27:06 -0300

    chore: path signup

--- src/Router/index.jsx
+++ src/Router/index.jsx
@@ -3,6 +3,7 @@ import Route from "./route";
 import { ToastContainer } from "react-toastify";
 import Home from "../Pages/Home";
 import Login from "../Pages/Login"
+import Signup from "../Pages/Signup"
 
 
 function Routes(){
@@ -11,6 +12,7 @@ function Routes(){
             <ToastContainer/>
             <Switch>
                 <Route exact path={"/"} component={Home}/>
+                <Route path={"/cadastrar"} component={Signup}/>
                 <Route path={"/login"} component={Login}/>
             </Switch>
         </div>
