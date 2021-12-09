48d2d14a79ef923dfba90524089c6898e109ac4f
sidny <sidny.silva@outlook.com>
2021-12-09 14:28:02 -0300

    style: responsive Home image

--- src/Pages/Home/style.js
+++ src/Pages/Home/style.js
@@ -1,70 +1,79 @@
 import styled from "styled-components";
 
 export const Container = styled.div`
-    height: 100%;
-    background-color: var(--dark-purple);
-    padding: 20px 20px 0px 20px;
-    box-sizing: border-box;
-    @media (min-width:1190px){
-        background-image: linear-gradient(to right, var(--light-green) 70%, var(--dark-purple) 30%);
-        overflow-y: hidden;
-        height: 100vh;
-    }
-    
+  height: 100%;
+  background-color: var(--dark-purple);
+  padding: 20px 20px 0px 20px;
+  box-sizing: border-box;
+  @media (min-width: 1015px) {
+    background-image: linear-gradient(
+      to right,
+      var(--light-green) 70%,
+      var(--dark-purple) 30%
+    );
+    overflow-y: hidden;
+    height: 100vh;
+  }
 `;
 export const DivInfos = styled.div`
-    max-width: 300px;
-    p{
-        font-family: 'Anton', sans-serif;
-        color: var(--white);    
-        font-size: 50px;
-        margin-top: 100px;
-    }
-    
+  max-width: 300px;
+  p {
+    font-family: "Anton", sans-serif;
+    color: var(--white);
+    font-size: 50px;
+    margin-top: 100px;
+  }
 `;
 
 export const DivLinks = styled.div`
-    margin-top: 30px;
-    a{
-        display:flex;
-        justify-content: flex-end;
-        font-family: 'Dancing Script', cursive;
-        text-decoration: none;
-        font-size: 30px;
-        color: var(--white);
-        margin-top: 10px;
-        
-    }
+  margin-top: 30px;
+  a {
+    display: flex;
+    justify-content: flex-end;
+    font-family: "Dancing Script", cursive;
+    text-decoration: none;
+    font-size: 30px;
+    color: var(--white);
+    margin-top: 10px;
+  }
 `;
 
 export const ContainerAcess = styled.div`
-    display: flex;
-    justify-content: flex-end;
-    text-align: end;
-    overflow-y: hidden;
+  display: flex;
+  justify-content: flex-end;
+  text-align: end;
+  overflow-y: hidden;
 `;
 
 export const DivImage = styled.div`
-    display: flex;
-    align-items: flex-end;
-    height:100vh;
-    img{
-        display: none;
-    }
+  display: flex;
+  align-items: flex-end;
+  height: 100vh;
 
-    @media (min-width:1190px){
-        
-        img{    
-            display: block;
-            width: 850px;
-            margin: 0px;
-        }
+  img {
+    display: none;
+  }
+
+  @media (min-width: 1015px) {
+    img {
+      display: flex;
+      max-width: 700px;
+      max-height: 600px;
+      margin-right: 0.5em;
+      margin-bottom: 4em;
+      object-fit: cover;
+      object-position: center;
     }
-    @media (min-width:1295px){
-        img{
-            display: block;
-            width: 850px;
-            margin-right:10px;
-        }
+  }
+  @media (min-width: 1295px) {
+    img {
+      display: flex;
+      max-width: 700px;
+      max-height: 900px;
+      margin-right: 5em;
+      margin-bottom: 4em;
+      object-fit: cover;
+      object-position: center;
     }
+  }
 `;
