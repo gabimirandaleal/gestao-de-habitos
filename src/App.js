1c54efd610e6cab10ee166a9b55b2cd5e4f4037c
Gabriela Leal <gabrielamiranda@alu.ufc.br>
2021-12-10 10:51:42 -0300

    refactor: add route in app.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
-import PopUpCreateActivity from "./Components/PopUpCreateActivity";
-
 import Routes from "./Router";
 
 function App() {
   return (
     <div className="App">
-      <PopUpCreateActivity></PopUpCreateActivity>
+      <Routes></Routes>
     </div>
   );
 }
