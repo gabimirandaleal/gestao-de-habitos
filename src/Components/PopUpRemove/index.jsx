8a30a477a7e82dc47e1efb19066fd8b137498379
Gabriela Leal <gabrielamiranda@alu.ufc.br>
2021-12-10 12:15:44 -0300

    feat: popup remove

---
+++ src/Components/PopUpRemove/index.jsx
@@ -0,0 +1,30 @@
+import ButtonYesOrNo from "../ButtonYesOrNo"
+import {DivA, DivContainer, Div} from "./style"
+
+function PopUpRemove ({text}) {
+
+  function removerCard(){
+    if(text === "hábito"){
+
+    }else if(text === "meta"){
+
+    }else if(text === "grupo"){
+      
+    }else if(text === "atividade"){
+      
+    }
+  }
+
+  return (
+    <DivA>
+      <DivContainer> 
+          <Div>
+              <h3>Remover {text}?</h3>
+              <ButtonYesOrNo></ButtonYesOrNo>
+          </Div>
+      </DivContainer>
+    </DivA>
+  )
+}
+
+export default PopUpRemove;
