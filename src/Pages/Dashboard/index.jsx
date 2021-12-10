caf02e12681127be390ad8b5502d06930a88b2ac
Pablo Rocha Goncalves <pablorgoncalves@gmail.com>
2021-12-10 12:49:30 -0300

    style: logout button

--- src/Pages/Dashboard/index.jsx
+++ src/Pages/Dashboard/index.jsx
@@ -2,28 +2,35 @@ import { HeaderDashboard, ButtonsDashboard, UserBox, Container } from "./style"
 import { useState } from "react"
 import Header from "../../Components/Header"
 
+import { useDispatch } from "react-redux";
+import {logOutThunk} from "../../Store/modules/user/thunk"
+import { FiLogOut } from "react-icons/fi";
+import { BsPencil } from "react-icons/bs";
+
 import GroupsImg from '../../assets/img/groups.png'
 import HabitsImg from '../../assets/img/habits.png'
 import UserImg from '../../assets/img/userAvatar.png'
-import Pencil from '../../assets/img/pencil.png'
 
 
 const Dashboard = () => {
   
   const [isHabits,setIsHabits] = useState(false)
   const [isGroup,setIsGroup] = useState(false)
+  const dispatch = useDispatch()
 
   const groups = () => {
-  setIsGroup(true)
-  setIsHabits(false)
-
+    setIsGroup(true)
+    setIsHabits(false)
   } 
 
   const habits = () => {
     setIsGroup(false)
     setIsHabits(true)
-  
     } 
+  
+  const logout = () => {
+    dispatch(logOutThunk())
+  }
 
   return (
 
@@ -41,11 +48,14 @@ const Dashboard = () => {
         <UserBox>
           <img src={UserImg} alt=''/>
           <ul>
-            <li><span>Username</span> <img src={Pencil} alt=''/></li>
+            <li><span>Username</span> <BsPencil/></li>
             <li>username@teste.com</li>
           </ul>
           
         </UserBox>
+
+        <FiLogOut onClick={logout}/>
+        
       </HeaderDashboard>
       <Container>
             {isHabits && <>Hábitos</> }
